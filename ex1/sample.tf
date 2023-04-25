bc2d531078877d2eecdcbcdd6dadda29695718fe
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-25 12:37:23 +0530

    terraform

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = ami-0b5a2b5b8f2be4ec2
+  ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "workstation-sample"
+    Name = "HelloWorld"
   }
 }
