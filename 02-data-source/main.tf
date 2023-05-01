d6ec59236ccca3d20ea0c4d0b279498e6f74c431
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-01 21:29:47 +0530

    front end public ip

--- 02-data-source/main.tf
+++ 02-data-source/main.tf
@@ -1,7 +1,14 @@
-data "aws_security_group" "selected" {
-  name = "allow-all"
-}
+//data "aws_security_group" "selected" {
+// name = "allow-all"
+//}
 
-output  "security_group_id" {
-        value = data.aws_security_group.selected.id
+//output  "security_group_id" {
+//value = data.aws_security_group.selected.id
+//}
+
+
+data "aws_security_groups" "test" {}
+
+output "all_sg" {
+  value = data.aws_security_groups.test
 }
