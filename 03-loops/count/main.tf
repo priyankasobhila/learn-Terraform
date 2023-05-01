24ce0eaefc788453a082c65af475a1154c5d8a9f
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-01 21:58:37 +0530

    front end public ip

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -4,6 +4,10 @@ resource  "null_resource" "null" {
 
 resource  "null_resource" "fruits" {
   count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "echo fruit name - $(var.fruits[count.index])"
+  }
 }
  variable "fruits" {
    default = ["apple", "mango", "banana"]
