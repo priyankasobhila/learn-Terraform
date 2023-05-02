2a7ab8ca55ee08b59288b0e339d6c999aa7fafb8
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-02 21:51:45 +0530

    front end public ip

--- 03-loops/for-each/main.tf
+++ 03-loops/for-each/main.tf
@@ -1,10 +1,15 @@
 resource  "null_resource" "fruits" {
-  count = length(var.fruits)
+  //count = length(var.fruits)
 
   provisioner "local-exec" {
-    command = "echo Fruit Name - ${var.fruits[count.index]}"
+    //command = "echo Fruit Name - ${var.fruits[count.index]}"
+    command = "echo ${length(var.fruits)}"
   }
 }
 variable "fruits" {
-  default = ["apple", "mango", "banana"]
+  default = {
+    apple = 10
+    orange = 200
+    banana = 100
+  }
 }
