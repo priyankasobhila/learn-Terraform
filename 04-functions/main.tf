4508245fb13c1f74cc375c2f220476ce8d4fb9dc
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-05-04 00:10:46 +0530

    addes

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -13,3 +13,14 @@ variable "sample1" {
 output "sample1"{
   value = element(var.sample1, 0)
 }
+
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+  }
+}
+
+output "sample2"{
+  value = lookup(var.sample1, "a1", "dummy")
+}
